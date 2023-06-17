04b5acd55a12d251fbce477c210fd604cce5bb3d
Morine Achieng <morineachieng@Morines-MacBook-Air.local>
2023-06-17 22:12:45 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+const leastFavoriteCustomer = 'George'
+
+function upperCaseCustomerName(customerName){
+    return customerName.toUpperCase()
+
+}
+
+function setBestCustomer(bestCustomer){
+    return bestCustomer = 'not bob'
+
+}
+
+function overwriteBestCustomer(bestCustomer){
+    return bestCustomer = 'maybe bob'
+}
+
+function changeLeastFavoriteCustomer(leastFavoriteCustomer) {
+    return leastFavoriteCustomer = 'Jane'
+    
+}
